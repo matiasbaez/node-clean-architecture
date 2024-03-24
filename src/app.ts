0b37751da28ce55dff9ac1486e8ab68a21af58e1
Matias Baez <matiasbaez2512@gmail.com>
2024-03-24 14:31:24 -0400

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import { envs } from "./config/envs";
+import { AppRoutes } from "./presentation/routes";
+import { Server } from "./presentation/server";
+
+(() => {
+    main();
+})();
+
+function main() {
+
+    // Start web server
+    const server = new Server({
+        port: envs.PORT,
+        publicPath: envs.PUBLIC_PATH,
+        routes: AppRoutes.routes
+    });
+
+    server.start();
+
+}
---
+++ src/presentation/server.ts
@@ -0,0 +1,43 @@
+import express, { Router } from "express";
+// import path from "path";
+
+interface Options {
+    port: number;
+    routes: Router;
+    publicPath?: string;
+}
+
+export class Server {
+
+    private app = express();
+    private readonly port: number;
+    private readonly publicPath: string;
+    private readonly routes: Router;
+
+    constructor(options: Options) {
+        const { port, publicPath = "public", routes } = options;
+        this.port = port;
+        this.publicPath = publicPath;
+        this.routes = routes;
+    }
+
+    start() {
+
+        this.app.use( express.static(this.publicPath) );
+
+        // API Routes
+        this.app.use( this.routes );
+
+        // SPA
+        // this.app.get("*", (req, res) => {
+        //     const indexPath = path.join(`${__dirname}../../../public/index.html`);
+        //     return res.sendFile(indexPath);
+        // });
+
+        this.app.listen(this.port, () => {
+            console.log("Server started on port 3000");
+        });
+
+    }
+
+}
